220bdf0488046ee07d8351d09abe4b9f9ed9bf25
Deyan Yonkov <deqnyonkov47@gmail.com>
2023-09-04 01:12:31 +0300

    Sending data to firebase

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,9 +4,12 @@ import classes from "./Cart.module.css";
 import CartContext from "../../store/cart-context";
 import CartItem from "./CartItem";
 import Checkout from "./Checkout";
+import Spinner from "../Spinner/Spinner";
 
 const Cart = (props) => {
   const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
   const ctx = useContext(CartContext);
 
   const totalAmount = `$${ctx.totalAmount.toFixed(2)}`;
@@ -37,6 +40,21 @@ const Cart = (props) => {
     </div>
   );
 
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true);
+    const response = await fetch(process.env.REACT_APP_ORDERS, {
+      method: "POST",
+      body: JSON.stringify({ user: userData, orderedItems: ctx.items }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
+    setIsSubmitting(false);
+    setDidSubmit(true);
+    ctx.clearCart();
+  };
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {ctx.items.map((item) => (
@@ -51,16 +69,45 @@ const Cart = (props) => {
       ))}
     </ul>
   );
-  return (
-    <Modal onClose={props.onClose}>
+
+  const cartModalContent = (
+    <>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onCancel={props.onClose} />}
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+      )}
       {!isCheckout && modalActions}
+    </>
+  );
+
+  const isSubmittingModalContent = (
+    <>
+      <Spinner />
+      <p>Sending order data</p>
+    </>
+  );
+
+  const didSubmitModalContent = (
+    <>
+      <p>Successfully sent order!</p>
+      <div className={classes.actions}>
+        <button className={classes.button} onClick={props.onClose}>
+          Close
+        </button>
+      </div>
+    </>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {!isSubmitting && !didSubmit && cartModalContent}
+      {isSubmitting && isSubmittingModalContent}
+      {!isSubmitting && didSubmit && didSubmitModalContent}
     </Modal>
   );
 };
-export default Cart;
+export default React.memo(Cart);
