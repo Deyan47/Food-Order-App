fcbbfbe81091b474f4683e637567d7e8d02a05f7
Deyan Yonkov <deqnyonkov47@gmail.com>
2023-09-03 23:52:22 +0300

    Checkout form & validation

---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,98 @@
+import { useRef, useState } from "react";
+import classes from "./Checkout.module.css";
+
+const isEmpty = (value) => value.trim() === "";
+const isNotPostalCode = (value) => value.trim().length !== 4;
+
+const Checkout = (props) => {
+  const [formInputsValid, setFormInputsValid] = useState({
+    name: true,
+    street: true,
+    city: true,
+    postalCode: true,
+  });
+  const nameInput = useRef();
+  const streetInput = useRef();
+  const postalCodeInput = useRef();
+  const cityInput = useRef();
+
+  const confirmHandler = (event) => {
+    event.preventDefault();
+
+    const enteredName = nameInput.current.value;
+    const enteredStreet = streetInput.current.value;
+    const enteredPostalCode = postalCodeInput.current.value;
+    const enteredCity = cityInput.current.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredStreetIsValid = !isEmpty(enteredStreet);
+    const enteredCityIsValid = !isEmpty(enteredCity);
+    const enteredPostalCodeIsValid = !isNotPostalCode(enteredPostalCode);
+
+    setFormInputsValid({
+      name: enteredNameIsValid,
+      street: enteredStreetIsValid,
+      city: enteredCityIsValid,
+      postalCode: enteredPostalCodeIsValid,
+    });
+
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredStreetIsValid &&
+      enteredCityIsValid &&
+      enteredPostalCodeIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
+  };
+
+  const nameControlClasses = `${classes.control} ${
+    formInputsValid.name ? "" : classes.invalid
+  }`;
+
+  const streetControlClasses = `${classes.control} ${
+    formInputsValid.street ? "" : classes.invalid
+  }`;
+
+  const postalCodeControlClasses = `${classes.control} ${
+    formInputsValid.postalCode ? "" : classes.invalid
+  }`;
+
+  const cityControlClasses = `${classes.control} ${
+    formInputsValid.city ? "" : classes.invalid
+  }`;
+
+  return (
+    <form className={classes.form} onSubmit={confirmHandler}>
+      <div className={nameControlClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" ref={nameInput} />
+        {!formInputsValid.name && <p>Please enter a valid name</p>}
+      </div>
+      <div className={streetControlClasses}>
+        <label htmlFor="street">Street</label>
+        <input type="text" id="street" ref={streetInput} />
+        {!formInputsValid.street && <p>Please enter a valid street</p>}
+      </div>
+      <div className={postalCodeControlClasses}>
+        <label htmlFor="postal">Postal Code</label>
+        <input type="text" id="postal" ref={postalCodeInput} />
+        {!formInputsValid.postalCode && <p>Please enter a valid postal code</p>}
+      </div>
+      <div className={cityControlClasses}>
+        <label htmlFor="city">City</label>
+        <input type="text" id="city" ref={cityInput} />
+        {!formInputsValid.city && <p>Please enter a valid city</p>}
+      </div>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
+    </form>
+  );
+};
+
+export default Checkout;
